3d16481551b581e1d5d0b0c8c1c6d90c82effc9e
lucaluisi <luca07luisi@gmail.com>
2024-08-29 21:20:51 +0200

    some adjustments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import { useEffect } from "react";
-import Hero from "./ui/sections/Hero/Hero";
-import Pool from "./ui/sections/Pool/Pool";
-import RoomsRestaurant from "./ui/sections/RoomsRestaurant/RoomsRestaurant";
+import Hero from "../components/ui/sections/Hero/Hero";
+import Pool from "../components/ui/sections/Pool/Pool";
+import RoomsRestaurant from "../components/ui/sections/RoomsRestaurant/RoomsRestaurant";
 import Lenis from "lenis";
-import Bho from "./ui/sections/Bho/Bho";
-import Form from "./ui/sections/Form/Form";
-import Footer from "./ui/sections/Footer/Footer";
+import Bho from "../components/ui/sections/Bho/Bho";
+import Form from "../components/ui/sections/Form/Form";
+import Footer from "../components/ui/sections/Footer/Footer";
 
 export default function Home() {
   // smooth scroll
