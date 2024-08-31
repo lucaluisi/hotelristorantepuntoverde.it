2627853189b7c193bd946514e4ca704ce2666797
lucaluisi <luca07luisi@gmail.com>
2024-08-31 12:01:46 +0200

    rifaccio responsive e riaggiusto il video (sta volta bene spero)

--- src/components/ui/sections/Hero/Hero.tsx
+++ src/components/ui/sections/Hero/Hero.tsx
@@ -1,10 +1,8 @@
 import { H1 } from "@/components/ui/text";
-import { motion, useScroll, useTransform, circIn } from "framer-motion";
-import { useRef } from "react";
 
 export default function Hero() {
   return (
-    <div className="relative min-h-dvh overflow-hidden flex justify-center">
+    <div className="relative min-h-svh overflow-hidden flex justify-center">
       <H1 className="absolute bottom-10 z-20 text-center">
         Hotel Ristorante Punto Verde
       </H1>
@@ -12,7 +10,8 @@ export default function Hero() {
         autoPlay
         muted
         loop
-        className="-z-50 absolute w-auto min-w-full min-h-full max-w-none brightness-75"
+        playsInline
+        className="-z-50 absolute w-auto min-w-full min-h-svh max-w-none brightness-75"
       >
         <source src="video.webm" type="video/webm" />
         <source src="video.mp4" type="video/mp4" />
