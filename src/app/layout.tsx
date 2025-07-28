f919d5b025fe9d809ef1a1eff7f7b77d6f48ba9e
lucaluisi <luca07luisi@gmail.com>
2025-07-28 20:50:23 +0200

    setup favicon and icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,9 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+        <meta name="apple-mobile-web-app-title" content="Punto Verde" />
+      </head>
       <body className={`${inter.className} dark`}>
         <StickyCursor isMobile={mobile} />
         {children}
