bfa65fafae1daec74e89ddd9b38980d06d7bf7ac
lucaluisi <luca07luisi@gmail.com>
2026-02-03 18:34:10 +0100

    update tracking system URLs to use HTTPS

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,14 @@ const nextConfig = {
     output: "standalone",
     async rewrites() {
         return [
+            // https invece di http pk il redirect del reverse proxy da problemi
             {
                 source: '/tracking-system/script.js',
-                destination: `http://${process.env.STATS_SERVER}/script.js`,
+                destination: `https://${process.env.STATS_SERVER}/script.js`,
             },
             {
                 source: '/tracking-system/api/send',
-                destination: `http://${process.env.STATS_SERVER}/api/send`,
+                destination: `https://${process.env.STATS_SERVER}/api/send`,
             },
         ];
     },
