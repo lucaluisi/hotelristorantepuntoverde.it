3e806d50dd9896dd02b0bb01eef862382dbeeede
lucaluisi <luca07luisi@gmail.com>
2026-02-03 18:27:38 +0100

    add umami tracking via proxy rewrites

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: "standalone" };
+const nextConfig = {
+    output: "standalone",
+    async rewrites() {
+        return [
+            {
+                source: '/tracking-system/script.js',
+                destination: `http://${process.env.STATS_SERVER}/script.js`,
+            },
+            {
+                source: '/tracking-system/api/send',
+                destination: `http://${process.env.STATS_SERVER}/api/send`,
+            },
+        ];
+    },
+};
 
 export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Script from "next/script";
 
 export default async function RootLayout({
   children,
@@ -9,6 +10,12 @@ export default async function RootLayout({
     <html lang="en">
       <head>
         <meta name="apple-mobile-web-app-title" content="Punto Verde" />
+        <Script
+          async
+          src="/tracking-system/script.js"
+          data-host-url="/tracking-system"
+          data-website-id={`${process.env.UMAMI_WEBSITE_ID}`}
+        />
       </head>
       {children}
     </html>
